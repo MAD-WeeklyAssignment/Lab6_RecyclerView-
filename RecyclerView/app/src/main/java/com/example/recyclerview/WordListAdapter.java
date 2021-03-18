562068a854ff001e5294aa30ff6cc2e34eb8e145
Aditya8014 <adityapk60@gmail.com>
2021-03-18 07:13:44 +0545

    Storing your data in the adapter

--- RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
@@ -20,16 +20,38 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
     @Override
     public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View mItemView = mInflater.inflate(R.layout.wordlist_item, parent, false);
+
         return new WordViewHolder(mItemView, this);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mWordList.size();
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int mPosition = getLayoutPosition();
+            String element = mWordList.get(mPosition);
+            mWordList.set(mPosition, "Clicked " + element);
+            mAdapter.notifyDataSetChanged();
+        }
     }
 }
